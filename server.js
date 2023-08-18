8cd8ac06608d06da19c87be052ffa18cf5249afb
manav22 <manav2119@gmail.com>
2023-08-18 18:33:11 +0530

    post lottery working

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
- const { REDIS_URL } = process.env;
+
+
+const ulid = require("ulid");
+const { REDIS_URL } = process.env;
 const redis = require("redis");
 const client = redis.createClient({ url: REDIS_URL });
 // This is going to write any Redis error to console.
@@ -17,6 +20,51 @@ client.on("error", (error) => {
   // Send an empty object as the response.
   res.json({});
 });
+
+app.post("/lotteries", async (req, res) => {
+  const { type, name, prize } = req.body;
+  if (type !== "simple") {
+    res.status(422).json({ error: "Invalid lottery type" });
+    return;
+  }
+
+  if (typeof name !== "string" || name.length < 3) {
+    res.status(422).json({ error: "Invalid lottery name" });
+    return;
+  }
+
+  if (typeof prize !== "string" || prize.length < 3) {
+    res.status(422).json({ error: "Invalid lottery prize" });
+    return;
+  }
+
+const id = ulid.ulid();
+const newLottery = {
+  id,
+  name,
+  prize,
+  type,
+  status: "running",
+};
+
+
+  try {
+    await client.connect();
+
+    await client
+      .multi()
+      .hSet(`lottery.${id}`, newLottery)
+      .lPush("lotteries", id)
+      .exec();
+
+   await client.disconnect();
+   res.json(newLottery);
+ } catch (error) {
+  console.error(error);
+  res.status(500).json({ error: "Failed to create lottery" });
+  }
+});
+
 
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
