1fc43a49cccd6ead388695c34da3d3e2ab51c292
manav22 <manav2119@gmail.com>
2023-08-18 08:54:23 +0530

    added route to server.js and dev to script in package.json

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
   const app = express();
   const port = 3000;
   
+
+  app.get("/", (req, res) => {
+  // Send an empty object as the response.
+  res.json({});
+});
+  
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
   });
