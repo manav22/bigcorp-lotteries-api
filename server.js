5fba538b97de304daa803e6b9c4cee99cff65de8
manav22 <manav2119@gmail.com>
2023-08-18 09:58:54 +0530

    .env added

--- server.js
+++ server.js
@@ -2,12 +2,16 @@
   const app = express();
   const port = 3000;
   
+  const { REDIS_URL } = process.env;
+console.log(REDIS_URL);
+
+  app.use(express.json({ limit: '10kb' }));
 
   app.get("/", (req, res) => {
   // Send an empty object as the response.
   res.json({});
 });
-  
+
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
   });
